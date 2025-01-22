7b5f50120b9b1f77e1cfab30a1f6efcab10e7885
Mohammed Aeraf <aeraf@outlook.com>
2025-01-22 09:14:51 +0530

    Moved to archive folder

---
+++ js-problems.js
@@ -0,0 +1,12 @@
+let x = 10;
+x = "hello";
+
+console.log(x * 10);
+
+function calculateArea(length, width) {
+    return length * width;
+}
+
+console.log(calculateArea(5, 10)); // Valid usage: Outputs 50
+console.log(calculateArea(5, "10m")); // Logical issue: Outputs "510"
+console.log(calculateArea(5)); // Error at runtime: Outputs NaN
---
+++ ts-is-better.js
@@ -0,0 +1,8 @@
+function calculateArea(length, width) {
+    return length * width;
+}
+// Valid usage
+console.log(calculateArea(5, 10)); // Outputs 50
+// Compile-time errors:
+console.log(calculateArea(5, "10")); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+console.log(calculateArea(5)); // Error: Expected 2 arguments, but got 1.
